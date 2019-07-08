ddad2bd623aedd78c6f3ccb45152d59b7a1d598e
Orayb-AlSmadi <oraybalsmadi@yahoo.com>
2019-07-08 22:07:38 +0300

    function solution

--- day1/function.js
+++ day1/function.js
@@ -35,48 +35,56 @@ takes 2 arguments: age, amount per day.
 calculates the amount consumed for rest of the life (based on a constant max age).
 outputs the result to the screen like so: "You will need NN to last you until the ripe old age of X"
 */
-
-
+function calculateSupply (age , day){
+var maxAge = 70;
+  var z = maxAge-age;
+var w = day*z*365;
+return ("You will need "+ w +" to last you until the ripe old age of "+ maxAge)
+}
+console.log(calculateSupply (27 , 2000))
 /*
 4
 Write a function called greet that:
 takes 1 argument: name.
 and it will return hello + name
 */
+function greet(name){
+return ("hello "+ name)}
 
+console.log (greet("Ahmad"))
 
 /*
 5
 what is the error:
 // */
-// function double(cat) {
-//   return 2 * x;
-// }
+function double(x) {
+  return 2 * x;
+}
 
-// function double(7) {
-//   return 2 * 7;
-// }
+function double1(z) {
+  return 2 * z;
+}
 
-// function double('7') {
-//   return 2 * 'x';
-// }
+function double2(x) {
+  return 2 * x;
+}
 
 
 /*
 6
-fix these functions
-*/
-// func double1(x {
-//   return 2 * x ;
-// } 
+fix these functions */
 
-// functiondouble2 x)
-// return 2 * x; 
-// }
+function double1(x) {
+return 2 * x ;
+ } 
 
-// function (x) double3 {
-//   return 2 * x;
+function double2 (x) {
+return 2 * x; 
+}
 
+function  double3(x) {
+  return 2 * x;
+}
 
 /*
 7
@@ -84,8 +92,13 @@ Write a function called cube that:
 accept 1 parameter and calculate the cube of it
 */
 
+function cube (x)
+{
+  var z=x*x*x;
+  return z;
+}
 
-
+console.log(cube(5))
 
 /*
 
