801af64fab9b9a51ebb049fa29f8b0edb6d18746
orayb-alsmadi <oraybalsmadi@yahoo.com>
2019-07-08 17:56:36 +0300

    ex1

---
+++ day1/code.js
@@ -0,0 +1,64 @@
+//Ex#1
+
+console.log( 1+2);
+
+console.log( 1*5);
+
+console.log(4/2);
+
+console.log(3%2);
+
+console.log( 1+" text");
+
+console.log( "hello"+ "javaScript");
+
+console.log( "Goodbye"- "bye");
+
+console.log( "hi" * 3);
+
+
+//Ex#2 **********************
+ 
+var myAge= 28*365*28*60;
+console.log ("myAgeInMin" + myAge);
+
+//*************************** */
+console.log( "mod");
+
+console.log( 6%2);
+
+console.log(42%10);
+
+console.log(5%2);
+
+console.log(6%3);
+
+console.log(7%4);
+
+console.log(100%12);
+
+/************************** */
+
+var name= "Orayb ";
+var age= 28;
+console.log (name+ age)
+
+/************************** */
+
+var firstName= "Orayb ";
+var middleName= "Osama ";
+var lastName= "AlSmadi ";
+console.log (firstName + middleName + lastName)
+
+
+//Ex#3 **********************
+
+var animal = 'cat';
+var tenDivideBy3 = 10/3 ;
+var var5 = 'sbane5h';
+var animal= 'cat';
+var True = false;
+
+
+
+
---
+++ day1/function.js
@@ -0,0 +1,112 @@
+/*
+1
+Write a function named tellFortune that:
+takes 4 arguments: number of children, 
+partner's name, geographic location, job title.
+outputs your fortune to the screen like so: 
+"You will be a X in Y, and married to Z with N kids."
+*/
+
+function tellFortune(X, Y, Z, N) {
+  return console.log("You will be a " + X + " in " + Y + " , and married to " + Z + " with " + N + " kids.")
+}
+
+tellFortune("software Engineer", "newyork", "ahmad", 2)
+
+/*
+2
+Write a function named calculateDogAge that:
+takes 1 argument: your puppy's age.
+calculates your dog's age based on the conversion rate of 1 human year to 7 dog years.
+outputs the result to the screen like so: "Your doggie is NN years old in dog years!"
+*/
+
+function calculateDogAge (Z) {
+
+  return console.log("Your doggie is " + Z*7 + " years old in dog years!")
+}
+
+calculateDogAge (2)
+
+/*
+3
+Write a function named calculateSupply that:
+takes 2 arguments: age, amount per day.
+calculates the amount consumed for rest of the life (based on a constant max age).
+outputs the result to the screen like so: "You will need NN to last you until the ripe old age of X"
+*/
+
+
+/*
+4
+Write a function called greet that:
+takes 1 argument: name.
+and it will return hello + name
+*/
+
+
+/*
+5
+what is the error:
+// */
+// function double(cat) {
+//   return 2 * x;
+// }
+
+// function double(7) {
+//   return 2 * 7;
+// }
+
+// function double('7') {
+//   return 2 * 'x';
+// }
+
+
+/*
+6
+fix these functions
+*/
+// func double1(x {
+//   return 2 * x ;
+// } 
+
+// functiondouble2 x)
+// return 2 * x; 
+// }
+
+// function (x) double3 {
+//   return 2 * x;
+
+
+/*
+7
+Write a function called cube that:
+accept 1 parameter and calculate the cube of it
+*/
+
+
+
+
+/*
+
+
+
+*/
+
+/*
+
+
+
+*/
+
+/*
+
+
+
+*/
+
+/*
+
+
+
+*/
